830b471a570f5fdb31ab54d8d4c9e97e6c519f87
Fady Wagdy Atta <wagdyam73@yahoo.com>
2025-02-07 09:07:32 +0200

    Initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+function SoftwareDeveloper() {
+//   this.favoriteLanguage = 'JavaScript';
+// }
+// // console.log();
+// // Create an instance of SoftwareDeveloper
+// const fady = new SoftwareDeveloper();
+
+// // Access the property
+// console.log(SoftwareDeveloper.favoriteLanguage);// undefined
+// console.log(fady.favoriteLanguage); // Output: JavaScript
